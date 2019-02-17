87cd08b7a1ab8b5f74be5c97c890e597454928fe
konomae <konomae@users.noreply.github.com>
2019-02-17 19:47:52 +0900

    public

--- Sources/TrackedValue/TrackedValue.swift
+++ Sources/TrackedValue/TrackedValue.swift
@@ -1,20 +1,20 @@
 private final class Token {}
 
-struct TrackedValue<Value>: Equatable {
+public struct TrackedValue<Value>: Equatable {
     private var token: Token
 
-    var value: Value {
+    public var value: Value {
         didSet {
             self.token = Token()
         }
     }
 
-    init(_ value: Value) {
+    public init(_ value: Value) {
         self.token = Token()
         self.value = value
     }
 
-    static func == (lhs: TrackedValue<Value>, rhs: TrackedValue<Value>) -> Bool {
+    public static func == (lhs: TrackedValue<Value>, rhs: TrackedValue<Value>) -> Bool {
         return lhs.token === rhs.token
     }
 }
