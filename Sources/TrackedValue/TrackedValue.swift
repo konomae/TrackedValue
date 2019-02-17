74e68b8d2929bd5e5834e8592356a93d8cf24900
konomae <konomae@users.noreply.github.com>
2019-02-17 13:56:55 +0900

    Add initial implementation

--- Sources/TrackedValue/TrackedValue.swift
+++ Sources/TrackedValue/TrackedValue.swift
@@ -1,3 +1,20 @@
-struct TrackedValue {
-    var text = "Hello, World!"
+private final class Token {}
+
+struct TrackedValue<Value>: Equatable {
+    private var token: Token
+
+    var value: Value {
+        didSet {
+            self.token = Token()
+        }
+    }
+
+    init(_ value: Value) {
+        self.token = Token()
+        self.value = value
+    }
+
+    static func == (lhs: TrackedValue<Value>, rhs: TrackedValue<Value>) -> Bool {
+        return lhs.token === rhs.token
+    }
 }
--- Tests/TrackedValueTests/TrackedValueTests.swift
+++ Tests/TrackedValueTests/TrackedValueTests.swift
@@ -2,14 +2,13 @@ import XCTest
 @testable import TrackedValue
 
 final class TrackedValueTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(TrackedValue().text, "Hello, World!")
+    func testInit() {
+        let original = Bool.random()
+        let v = TrackedValue(original)
+        XCTAssertEqual(v.value, original)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testInit", testInit),
     ]
 }
