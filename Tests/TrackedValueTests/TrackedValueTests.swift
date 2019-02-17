d06726c054219139c0b0a773139fcc1c6c844350
konomae <konomae@users.noreply.github.com>
2019-02-17 14:14:12 +0900

    Add tests

--- Tests/TrackedValueTests/TrackedValueTests.swift
+++ Tests/TrackedValueTests/TrackedValueTests.swift
@@ -8,7 +8,38 @@ final class TrackedValueTests: XCTestCase {
         XCTAssertEqual(v.value, original)
     }
 
+    func testEqual() {
+        let v1 = TrackedValue(Bool.random())
+        let v2 = v1
+        XCTAssertEqual(v1, v2)
+    }
+
+    func testEqualWhenValueModifiedWithDifferentValue() {
+        var v1 = TrackedValue(1)
+        var v2 = v1
+
+        v1.value = 2
+        XCTAssertNotEqual(v1, v2)
+
+        v2.value = 3
+        XCTAssertNotEqual(v1, v2)
+    }
+
+    func testEqualWhenValueModifiedWithSameValue() {
+        var v1 = TrackedValue(1)
+        var v2 = v1
+
+        v1.value = 1
+        XCTAssertNotEqual(v1, v2)
+
+        v2.value = 1
+        XCTAssertNotEqual(v1, v2)
+    }
+
     static var allTests = [
         ("testInit", testInit),
+        ("testEqual", testEqual),
+        ("testEqualWhenValueModifiedWithDifferentValue", testEqualWhenValueModifiedWithDifferentValue),
+        ("testEqualWhenValueModifiedWithSameValue", testEqualWhenValueModifiedWithSameValue),
     ]
 }
